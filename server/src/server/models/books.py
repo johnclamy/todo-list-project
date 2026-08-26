455e4e6dc5f6244849fdfef500d516a9514baf99
johnclamy <johnclamy@gmail.com>
2026-08-26 21:53:36 +0100

    add a book publisher enum

--- server/src/server/models/books.py
+++ server/src/server/models/books.py
@@ -7,21 +7,29 @@ from typing import List, Optional
 
 # Define the allowed tech genres/categories
 class BookCategory(str, Enum):
-    SYSTEMS = "Fiction"
-    WEB_DEVELOPMENT = "Non-Fiction"
-    MOBILE = "Fantasy"
-    AI_AGENTS = "Science Fiction"
-    MYSTERY = "Mystery"
-    HISTORY = "History"
-    BIOGRAPHY = "Biography"
+    SYSTEMS = "Systems"
+    WEB_DEVELOPMENT = "Web Development"
+    MOBILE_DEVELOPMENT = "Mobile Development"
+    AI_AGENTS = "AI Agents"
+    MACHINE_LEARNING = "Machine Learning"
+    DATABASES = "Databases"
+    PROGRAMMING = "Programming"
+
+
+# Define the allowed tech publishers
+class BookPublisher(str, Enum):
+    MANNING = "Manning Publications Co"
+    OREILLEY = "O'Reilly Media Inc."
+    PACKT = "Packt Publishing Ltd"
 
 
 # Base schema (shared attributes)
 class BookBaseModel(BaseModel):
     isbn_13: str                     
-    title: str                        
+    title: str
+    subtitle: str                        
     authors: List[str]                
-    publisher: str                    
+    publisher: BookPublisher                    
     published_date: str               # Often 'YYYY' or 'YYYY-MM-DD' 
 
     # Recommended secondary fields
@@ -30,4 +38,3 @@ class BookBaseModel(BaseModel):
     # Enforces that every book in the list must be a valid BookCategory
     categories: List[BookCategory] = Field(default_factory=list)
     image_url: Optional[str] = None   # To display book covers in your UI
-
