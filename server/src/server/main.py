d5c9262ef9502c7c5a1a950e9d1e51b3f8814944
johnclamy <johnclamy@gmail.com>
2026-08-26 10:36:49 +0100

    include the books api router

--- server/src/server/main.py
+++ server/src/server/main.py
@@ -1,13 +1,10 @@
 import uvicorn
 from fastapi import FastAPI
+from server.api.v1.api import api_router
 
 
 app = FastAPI()
-
-
-@app.get('/books')
-def root() -> dict[str, str]:
-    return {"msg": "book list will go here"}
+app.include_router(api_router, prefix='/api/v1')
 
 
 if __name__ == "__main__":
