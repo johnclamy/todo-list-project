8fc115ad533c434b1bf83091cd7586a174c4cf7e
johnclamy <johnclamy@gmail.com>
2026-08-27 20:55:44 +0100

    consolidate the routes in router.py

--- server/src/server/api/v1/router.py
+++ server/src/server/api/v1/router.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
-from api.v1.endpoints import books
+from api.v1.endpoints import books, health, version
 
 
 api_router = APIRouter()
 
 
 # Include all endpoint routers
+api_router.include_router(version.router, tags=["version"])
+api_router.include_router(health.router, tags=["health"])
 api_router.include_router(books.router, tags=["books"])
