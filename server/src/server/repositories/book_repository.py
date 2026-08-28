cad5d748f8fa05fce489e64018a0367a29a0fe8a
johnclamy <johnclamy@gmail.com>
2026-08-28 17:30:07 +0100

    add full props to Book list

--- server/src/server/repositories/book_repository.py
+++ server/src/server/repositories/book_repository.py
@@ -4,33 +4,42 @@ from typing import List
 
 _books: List[BookBaseModel] = [
     BookBaseModel(
-        isbn_13="978-1-492-D4522-6",                    
+        isbn_13="9781492045526",                    
         title="Deep Learning for Coders with fastapi & PyTorch",
         subtitle="AI Applications Without a PhD",                      
         authors=["Jeremy Howard", "Sylvain Gugger"],             
-        publisher=BookPublisher.OREILLEY,               
+        publisher=BookPublisher.OREILLEY,
+        isbn_10=None,
+        page_count=None,               
         published_date="2020",
-        categories=[BookCategory.MACHINE_LEARNING, BookCategory.PROGRAMMING]
+        categories=[BookCategory.MACHINE_LEARNING, BookCategory.PROGRAMMING],
+        image_url=None
     ),
 
     BookBaseModel(
-        isbn_13="978-1-61729-223-1",                  
+        isbn_13="9781617292231",                  
         title="Grokking Algorithms",
         subtitle="An illustrated guide for programmers and other curious people",                      
         authors=["Aditya Y. Bhargava"],               
-        publisher=BookPublisher.MANNING,               
+        publisher=BookPublisher.MANNING,
+        isbn_10=None,
+        page_count=None,               
         published_date="2016",
-        categories=[BookCategory.PROGRAMMING]
+        categories=[BookCategory.PROGRAMMING],
+        image_url=None
     ),
 
     BookBaseModel(
-        isbn_13="978-0-596-52684-9",                    
+        isbn_13="9780596526849",                    
         title="Head First SQL",
         subtitle="A Brain-Friendly Guide",                      
         authors=["Lynn Beighley"],               
-        publisher=BookPublisher.OREILLEY,               
+        publisher=BookPublisher.OREILLEY,
+        isbn_10=None,
+        page_count=None,               
         published_date="2007",
-        categories=[BookCategory.DATABASES]
+        categories=[BookCategory.DATABASES],
+        image_url=None
     ),
 ]
 
