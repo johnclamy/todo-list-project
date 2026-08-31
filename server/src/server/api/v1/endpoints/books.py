897b8f4d8e66186e022700a27dfa2a1ed0b66f82
johnclamy <johnclamy@gmail.com>
2026-08-31 21:29:01 +0100

    add the required imports

--- server/src/server/api/v1/endpoints/books.py
+++ server/src/server/api/v1/endpoints/books.py
@@ -1,9 +1,18 @@
-from fastapi import APIRouter
+# server/src/server/api/v1/endpoints/books.py
+from fastapi import APIRouter, Depends, HTTPException, Query, status
+from sqlalchemy.orm import Session
+from typing import Optional, List
+from database.connection import get_db
+from models.books import (
+    BookCreateModel, BookUpdateModel, BookResponseModel,
+    BookListResponseModel, BookCategory, BookPublisher
+)
+from services.book_services import BookService
 
 
 router = APIRouter(prefix='/books')
 
 
 @router.get('/')
-def get_books() -> dict[str, str]:
+async def get_books() -> dict[str, str]:
     return {"msg": "book list will go here"}
